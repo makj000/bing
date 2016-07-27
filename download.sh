9a5f4ccdead2c03db27e55baa19d4d49b4797a4c
Kejia Ma <kma@oakm061649de1.corp.iparadigms.com>
2016-07-27 14:02:28 -0700

    2013/10/12 third checkin

--- download.sh
+++ download.sh
@@ -23,39 +23,41 @@ picExt=".jpg"
 # The mkt parameter determines which Bing market you would like to
 # obtain your images from.
 # Valid values are: en-US, zh-CN, ja-JP, en-AU, en-UK, de-DE, en-NZ, en-CA.
-mkt="zh-CN"
+for mkt in en-US zh-CN ja-JP en-AU en-UK de-DE en-NZ en-CA
+do
 
-# The idx parameter determines where to start from. 0 is the current day,
-# 1 the previous day, etc.
-idx="0"
+  # The idx parameter determines where to start from. 0 is the current day,
+  # 1 the previous day, etc.
+  for idx in 0 .. 20 
+  do
 
-   for picRes in _1920x1200 _1366x768 _1280x720 _1024x768 
+    for picRes in _1920x1200 _1366x768 _1280x720 _1024x768 
     do
 
-# $xmlURL is needed to get the xml data from which
-# the relative URL for the Bing pic of the day is extracted
-xmlURL="http://www.bing.com/HPImageArchive.aspx?format=xml&idx=$idx&n=1&mkt=$mkt"
+      # $xmlURL is needed to get the xml data from which
+      # the relative URL for the Bing pic of the day is extracted
+      xmlURL="http://www.bing.com/HPImageArchive.aspx?format=xml&idx=$idx&n=1&mkt=$mkt"
 
-    # Extract the relative URL of the Bing pic of the day from
-    # the XML data retrieved from xmlURL, form the fully qualified
-    # URL for the pic of the day, and store it in $picURL
-    picURI=$(echo $(curl -s $xmlURL) | grep -oP "<urlBase>(.*)</urlBase>" | cut -d ">" -f 2 | cut -d "<" -f 1)$picRes$picExt
-    picURL=$bing$picURI
-    picName=$(echo $picURI | cut -d "/" -f 5)
+      # Extract the relative URL of the Bing pic of the day from
+      # the XML data retrieved from xmlURL, form the fully qualified
+      # URL for the pic of the day, and store it in $picURL
+      picURI=$(echo $(curl -s $xmlURL) | grep -oP "<urlBase>(.*)</urlBase>" | cut -d ">" -f 2 | cut -d "<" -f 1)$picRes$picExt
+      picURL=$bing$picURI
+      picName=$(echo $picURI | cut -d "/" -f 5)
 
-    # $picName contains the filename of the Bing pic of the day
-    #picName=${picURL#*2f}
+      # $picName contains the filename of the Bing pic of the day
+      #picName=${picURL#*2f}
 
-    # Download the Bing pic of the day
-    curl --create-dirs -o $saveDir$picName $picURL
+      # Download the Bing pic of the day
+      curl --create-dirs -o $saveDir$picName $picURL
 
-    # Test if it's a pic
-    file $saveDir$picName | grep HTML && rm -rf $saveDir$picName && continue
+      # Test if it's a pic
+      file $saveDir$picName | grep HTML && rm -rf $saveDir$picName && continue
 
-    break
-   done
-    
-#done
+      break
+    done
+  done
+done
 
 # Exit the script
 exit 0
